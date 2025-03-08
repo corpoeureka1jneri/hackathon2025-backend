c983cfe8539b0438f5cfb041f8282ef619b5be9a
Christian Mendoza <christianmendozater@gmail.com>
2025-03-08 10:27:46 -0400

    Add entities.json with animal config

--- App/main.py
+++ App/main.py
@@ -5,3 +5,17 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+@app.get("/start")
+def start_sim(ticks: int):
+    
+    return {"message": "Simulation started"}
+
+
+@app.get("/download")
+def download_data():
+    return {"message": "Simulation stopped"}
+
+
+
+
